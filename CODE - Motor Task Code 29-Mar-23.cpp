99d7b9206939ccad6c87c7aa9439c206c6f5c24c
davidlukacik <127992961+davidlukacik@users.noreply.github.com>
2023-04-07 12:13:07 -0700

    Update CODE - Motor Task Code 29-Mar-23.cpp

--- CODE - Motor Task Code 29-Mar-23.cpp
+++ CODE - Motor Task Code 29-Mar-23.cpp
@@ -1,12 +1,12 @@
 #include <Stepper.h>
 
 const int stepsPerRevolution = 516;  // change this to fit the number of steps per revolution for your stepper motor
-const int ledPin = 13;  // the pin that the trial indicator LED is attached to
-const int irPin = 2;  // the pin that the IR Phototransistor is attached to
+const int ledPin = 13;  // the pin that the trial indicator LED is attached to (output)
+const int irPin = 2;  // the pin that the IR Phototransistor is attached to (input)
 unsigned long previousTime = 0;
-const unsigned long bufferTime = 30000; // 30 seconds buffer time between trials
+const unsigned long bufferTime = 30000; // 30 seconds buffer time between trials, changes this value to alter time between trials
 
-// initialize the stepper library on pins 9 through 12:
+// initialize the stepper library on pins 9 through 12 (ssee schematic):
 Stepper myStepper(stepsPerRevolution, 9, 10, 11, 12);
 
 void setup() {
@@ -28,22 +28,22 @@ void loop() {
     if (incomingByte == 't') {
       Serial.print("Trial started at: ");
       Serial.println(currentTime);
-      digitalWrite(ledPin, HIGH);  // turn on the LED
-      myStepper.setSpeed(50);  // set the speed of the stepper motor
-      myStepper.step(90);  // move the stepper motor 30 degrees
+      digitalWrite(ledPin, HIGH);  // turn on the indicator LED
+      myStepper.setSpeed(50);  // set the speed of the stepper motor 
+      myStepper.step(30);  // move the stepper motor 30 degrees
       previousTime = currentTime;
       delay(500);  // wait for half a second
     }
   }
 
-  // if the IR Phototransistor is blocked and either 30 seconds have passed since the last activation
+  // if the IR Phototransistor is tripped and either 30 seconds have passed since the last activation
   // or a trial has been triggered by a serial input, turn on the LED and move the stepper motor 30 degrees
   if (irState == LOW && (currentTime - previousTime > bufferTime || Serial.available() > 0)) {
     Serial.print("Trial started at: ");
     Serial.println(currentTime);
     digitalWrite(ledPin, HIGH);  // turn on the LED
     myStepper.setSpeed(50);  // set the speed of the stepper motor
-    myStepper.step(90);  // move the stepper motor 30 degrees
+    myStepper.step(30);  // move the stepper motor 30 degrees
     previousTime = currentTime;
     delay(500);  // wait for half a second
   } else {

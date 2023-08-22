b195a70fecbd57d58c0b9b1f4c2867b98b3bf98b
davidlukacik <127992961+davidlukacik@users.noreply.github.com>
2023-08-22 15:33:15 -0700

    Update ADMotorTask Video Recording.py

--- ADMotorTask Video Recording.py
+++ ADMotorTask Video Recording.py
@@ -1,7 +1,8 @@
+# This runs on Python version 3.7
 """
-Created on June 20, 2023
+Created on Tue Aug 22 15:11:50 2023
 
-@author: verpeutlab, davidlukacik
+@author: verpeutlab
 """
 
 import os
@@ -11,25 +12,21 @@ from PySpin import PySpin
 import subprocess
 import serial
 
-# Set the save location for the videos (Edit this to correspond with where you want the videos saved!)
-save_folder = "C:/Users/verpeutlab/Videos/pyspintest"
+# Set the save location for the videos
+save_folder = "D:\FineMotorVideos\PySpinTestTemp"
 
-# Set the desired frames per second (This would be 120 in an ideal world, but Spinnaker is stubborn)
+# Set the desired frames per second
 fps = 32
 
-# Set the recording duration in seconds
-recording_duration = 30
-
-# Set the serial port and baud rate. Be sure to double check the serial port the Arduino is plugged into!
+# Set the serial port and baud rate
 serial_port = "COM3"
 baud_rate = 9600
 
+# Specify the path to the ffplay executable
+ffplay_path = r"C:\Users\verpeutlab\Documents\ff\ffplay.exe"
+
 # Initialize serial communication
 ser = serial.Serial(serial_port, baud_rate)
-
-# Wait for the "cr" signal from Arduino to start the recording. This may be slightly redundant, at least the corresponding code in the Arduino program. 
-while ser.read(2) != b"cr":
-    pass
 
 # Initialize PySpin system
 system = PySpin.System.GetInstance()
@@ -38,64 +35,94 @@ cam_list = system.GetCameras()
 if cam_list.GetSize() == 0:
     print("No cameras found.")
     system.ReleaseInstance()
+    ser.close()  # Close serial communication
     exit(1)
 
-# Select the first camera
-camera = cam_list.GetByIndex(0)
-
-# Initialize camera
-camera.Init()
-
-# Configure camera settings
-camera.AcquisitionMode.SetValue(PySpin.AcquisitionMode_Continuous)
-camera.AcquisitionFrameRateEnable.SetValue(True)
-camera.AcquisitionFrameRate.SetValue(fps)
-
-# Start the camera stream
-camera.BeginAcquisition()
-
-# Create a VideoWriter object to save the recording
-current_time = time.strftime("%Y%m%d_%H%M%S")
-video_name = os.path.join(save_folder, f"pyspintest_recording_{current_time}.avi")
-
-# Define the codec and create a VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-video_writer = cv2.VideoWriter(video_name, fourcc, fps, (camera.Width.GetValue(), camera.Height.GetValue()))
-
-# Record for the specified duration
-start_time = time.time()
-while time.time() - start_time < recording_duration:
+# Function to initialize and start the camera
+def init_and_start_camera():
     try:
-        # Get the next image from the camera
-        image_result = camera.GetNextImage()
+        # Select the first camera
+        camera = cam_list.GetByIndex(0)
 
-        # Convert the image to a numpy array
-        image_data = image_result.GetNDArray()
+        # Initialize camera
+        camera.Init()
 
-        # Convert the image to BGR format for compatibility with OpenCV
-        image_bgr = cv2.cvtColor(image_data, cv2.COLOR_GRAY2BGR)
+        # Configure camera settings
+        camera.AcquisitionMode.SetValue(PySpin.AcquisitionMode_Continuous)
+        camera.AcquisitionFrameRateEnable.SetValue(True)
+        camera.AcquisitionFrameRate.SetValue(fps)
 
-        # Write the image frame to the video file
-        video_writer.write(image_bgr)
+        # Start the camera stream
+        camera.BeginAcquisition()
 
-        # Release the image
-        image_result.Release()
+        return camera
+
+    except PySpin.SpinnakerException as e:
+        print(f"Error initializing camera: {e}")
+        return None
+
+# Function to start recording for a specified duration
+def start_recording(camera, duration):
+    try:
+        # Create a VideoWriter object to save the recording
+        current_time = time.strftime("%Y%m%d_%H%M%S")
+        video_name = os.path.join(save_folder, f"pyspintest_recording_{current_time}.avi")
+
+        # Define the codec and create a VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+        video_writer = cv2.VideoWriter(video_name, fourcc, fps, (camera.Width.GetValue(), camera.Height.GetValue()))
+
+        # Record for the specified duration
+        start_time = time.time()
+        while time.time() - start_time < duration:
+            try:
+                # Get the next image from the camera
+                image_result = camera.GetNextImage()
+
+                # Convert the image to a numpy array
+                image_data = image_result.GetNDArray()
+
+                # Convert the image to BGR format for compatibility with OpenCV
+                image_bgr = cv2.cvtColor(image_data, cv2.COLOR_GRAY2BGR)
+
+                # Write the image frame to the video file
+                video_writer.write(image_bgr)
+
+                # Release the image
+                image_result.Release()
+
+            except PySpin.SpinnakerException as e:
+                print(f"Error: {e}")
+                break
+
+        # End the camera acquisition
+        camera.EndAcquisition()
+
+        # Release the video writer
+        video_writer.release()
 
     except PySpin.SpinnakerException as e:
         print(f"Error: {e}")
-        break
 
-# Release the camera and system resources
-camera.EndAcquisition()
-camera.DeInit()
+# Main loop
+while True:
+    # Wait for the "cr" signal from Arduino to start the recording
+    while ser.read(2) != b"cr":
+        pass
+
+    # Initialize and start the camera
+    camera = init_and_start_camera()
+
+    if camera is not None:
+        # Start recording for 30 seconds
+        start_recording(camera, 30)
+
+        # Close the camera
+        camera.DeInit()
+
+# Close serial communication
+ser.close()
+
+# Release the camera list and system resources
 cam_list.Clear()
 system.ReleaseInstance()
-
-# Release the video writer
-video_writer.release()
-
-# Launch the video window using subprocess 
-video_window = subprocess.Popen(["ffplay", "-i", video_name])
-
-# Wait for the video window to close
-video_window.wait()
